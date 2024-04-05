ae4acb85f904adb8c3bbbbca147b58af59a69301
Philippe SKOPAL <pskopal@nevaone.int>
2024-04-05 09:40:37 +0200

    Add shallow calc functions, and unit tests

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static int add(int a, int b) {
+        return 0;
+    }
+
+    public static int sub(int a, int b) {
+        return 0;
+    }
+}
---
+++ src/CalcTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+    @Test
+    @DisplayName("Add two numbers")
+    void add() {
+        assertEquals(4, Calc.add(2, 2));
+    }
+}

bb7a46f64aef3ab39b89345307e950c85d87abb2
Philippe SKOPAL <pskopal@nevaone.int>
2024-04-05 09:44:28 +0200

    Unit test passes, function add

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,9 @@
 public class Calc {
     public static int add(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     public static int sub(int a, int b) {
-        return 0;
+        return a - b;
     }
 }
